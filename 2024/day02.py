110fc318c45d0ae3b383e0b279426d08436925a8
Erik Lokhorst <erik@pitts2.onmicrosoft.com>
2024-12-02 20:30:37 +0100

    fix

--- 2024/day02.py
+++ 2024/day02.py
@@ -1,3 +1,6 @@
+import time
+
+
 def is_consistent(report: list[int]) -> bool:
     return report == sorted(report) or report == sorted(report, reverse=True)
 
@@ -17,22 +20,26 @@ def determine_safety(report: list[int]) -> int:
 def determine_tolerated_safety(report: list[int]) -> int:
     if determine_safety(report):
         return 1
-    for level_leftout in report:
-        if determine_safety([level for level in report if level != level_leftout]):
+    for i in range(len(report)):
+        list_leftout = report[:i] + report[i + 1 :]
+        if determine_safety(list_leftout):
             return 1
     return 0
 
 
 def run_reports(file: str) -> tuple[int, int]:
+    start = time.time()
     reports = [
         list(map(int, line.split())) for line in open(file, "r", encoding="utf-8")
     ]
     safe_count = sum([determine_safety(report) for report in reports])
     tolerated_count = sum([determine_tolerated_safety(report) for report in reports])
+    end = time.time()
+    print(f"Total time: {(end-start) * 10**3}")
     return safe_count, tolerated_count
 
 
-res_example = run_reports("day2_example.csv")
+res_example = run_reports("2024/day02_example.csv")
 print(res_example)
-res_actual = run_reports("day2_input.csv")
+res_actual = run_reports("2024/day02_input.csv")
 print(res_actual)
