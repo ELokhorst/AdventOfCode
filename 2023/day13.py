083573441fec598b15a09b24a6fe829af19e6380
Erik Lokhorst <erik@pitts2.onmicrosoft.com>
2024-12-05 08:49:26 +0100

    another try at 2023 13

--- 2023/day13.py
+++ 2023/day13.py
@@ -22,21 +22,33 @@ def read_patterns(file: str):
 
 
 def check_mirror_idx(pattern: list[str], idx: int) -> int:
+    print(f"Checking for mirror index: {idx} / {len(pattern)}")
     i = 0
+
     while True:
         left_index = idx - i
         right_index = idx + i + 1
-        print(f"Check mirror for {left_index} and {right_index}")
+
         if left_index < 0 or right_index >= len(pattern):
+            print(
+                f"Reached {'left' if left_index < 0 else 'right'} boundary for index: {idx}"
+            )
             return idx + 1
+
         if pattern[left_index] != pattern[right_index]:
-            break
+            print(pattern[left_index])
+            print(pattern[right_index])
+            print(
+                f"Mismatch at left_index: {left_index} and right_index: {right_index}"
+            )
+            return 0
+
         i += 1
-    return 0
 
 
 def find_mirroring(pattern: list[str]) -> int:
     is_even = len(pattern) % 2 == 0
+    print(f"Even pattern: {is_even}")
     mirror_index = len(pattern) // 2 - 1
     check = check_mirror_idx(pattern, mirror_index)
     if check:
@@ -52,16 +64,24 @@ def summarize_notes(file: str) -> int:
     h, v = read_patterns(file)
 
     totals = 0
-    for h_p, v_p in zip(h, v):
-        mirror_v = find_mirroring(v_p)
+    for h_p in h:
+        print("Horizontal pattern")
+        [print(line) for line in h_p]
         mirror_h = find_mirroring(h_p)
+        print(f"Adding {100 * mirror_h}")
         totals += 100 * mirror_h
+
+    for v_p in v:
+        print("Vertical pattern")
+        [print(line) for line in v_p]
+        mirror_v = find_mirroring(v_p)
+        print(f"Adding {mirror_v}")
         totals += mirror_v
 
     return totals
 
 
-res_example = summarize_notes("day13_example.txt")
+res_example = summarize_notes("2023/day13_example.txt")
 print(res_example)
-res_actual = summarize_notes("day13_input.txt")
+res_actual = summarize_notes("2023/day13_input.txt")
 print(res_actual)
