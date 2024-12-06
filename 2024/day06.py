539f3bf201f4bc8948407223aabb4979f09b61dd
Erik Lokhorst <erik@pitts2.onmicrosoft.com>
2024-12-06 11:27:28 +0100

    day6 part 2 inefficient

--- 2024/day06.py
+++ 2024/day06.py
@@ -44,67 +44,70 @@ def distance_to_edge(coords, guard_pos, direction):
 def update_guard_pos(coords, objects, guard_pos, direction, visited: list):
     obstacle = find_obstacle(objects, guard_pos, direction)
     if not obstacle:
-        visited.extend(distance_to_edge(coords, guard_pos, direction))
-        return guard_pos, False
-    visited.extend(
-        [
-            (x, y)
-            for (x, y), _ in coords
-            if (
-                direction == "up"
-                and x == obstacle[0] == guard_pos[0]
-                and obstacle[1] < y <= guard_pos[1]
-            )
-            or (
-                direction == "right"
-                and y == obstacle[1] == guard_pos[1]
-                and guard_pos[0] <= x < obstacle[0]
-            )
-            or (
-                direction == "down"
-                and x == obstacle[0] == guard_pos[0]
-                and guard_pos[1] <= y < obstacle[1]
-            )
-            or (
-                direction == "left"
-                and y == obstacle[1] == guard_pos[1]
-                and obstacle[0] < x <= guard_pos[0]
-            )
-        ]
-    )
+        # visited.extend(distance_to_edge(coords, guard_pos, direction))
+        return (
+            (guard_pos, None),
+            False,
+        )
+    # visited.extend(
+    #     [
+    #         (x, y)
+    #         for (x, y), _ in coords
+    #         if (
+    #             direction == "up"
+    #             and x == obstacle[0] == guard_pos[0]
+    #             and obstacle[1] < y <= guard_pos[1]
+    #         )
+    #         or (
+    #             direction == "right"
+    #             and y == obstacle[1] == guard_pos[1]
+    #             and guard_pos[0] <= x < obstacle[0]
+    #         )
+    #         or (
+    #             direction == "down"
+    #             and x == obstacle[0] == guard_pos[0]
+    #             and guard_pos[1] <= y < obstacle[1]
+    #         )
+    #         or (
+    #             direction == "left"
+    #             and y == obstacle[1] == guard_pos[1]
+    #             and obstacle[0] < x <= guard_pos[0]
+    #         )
+    #     ]
+    # )
     if direction == "up":
-        return (obstacle[0], obstacle[1] + 1), True
+        return ((obstacle[0], obstacle[1] + 1), direction), True
     elif direction == "right":
-        return (obstacle[0] - 1, obstacle[1]), True
+        return ((obstacle[0] - 1, obstacle[1]), direction), True
     elif direction == "down":
-        return (obstacle[0], obstacle[1] - 1), True
+        return ((obstacle[0], obstacle[1] - 1), direction), True
     elif direction == "left":
-        return (obstacle[0] + 1, obstacle[1]), True
+        return ((obstacle[0] + 1, obstacle[1]), direction), True
 
 
 def walkthrough(coords, objects):
     directions = ["up", "right", "down", "left"]
     i = 0
-    guard_pos = [coords for coords, token in coords if token == "^"][0]
-    guard_positions = []
+    guard_pos = ([coords for coords, token in coords if token == "^"][0], None)
+    guard_positions = set()
     visited = []
 
     loop = False
     while True:
         direction = directions[i]
         guard_pos, continue_loop = update_guard_pos(
-            coords, objects, guard_pos, direction, visited
+            coords, objects, guard_pos[0], direction, visited
         )
 
         if not continue_loop:
             break
 
         if guard_pos in guard_positions:
-            print("Found loop")
+            print(f"Found loop for {guard_pos} in {guard_positions}")
             loop = True
             break
 
-        guard_positions.append(guard_pos)
+        guard_positions.add(guard_pos)
         i = (i + 1) % 4
 
     return loop
@@ -116,11 +119,14 @@ def main(file: str):
 
     layout = content.split("\n")
     coords_token = [[(i, j), m] for j, l in enumerate(layout) for i, m in enumerate(l)]
-    objects = [coords for coords, token in coords_token if token == "#"]
+    print(f"Grid is: {len(layout)} * {len(layout[0])}")
 
     loop = 0
-    for coord, _ in coords_token:
-        objects.append(coord)
+    for coord, token in coords_token:
+        if not token == ".":
+            continue
+        # print(f"Creating object at: {coord}")
+        objects = [coords for coords, token in coords_token if token == "#"] + [coord]
         result = walkthrough(coords_token, objects)
         if result:
             loop += 1
@@ -130,5 +136,5 @@ def main(file: str):
 
 res_example = main("2024/day06_example.txt")
 print(res_example)
-# res_actual = main("2024/day06_input.txt")
-# print(res_actual)
+res_actual = main("2024/day06_input.txt")
+print(res_actual)
