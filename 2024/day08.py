583d966e12daec5d22ff84932ca56e1efd38ef5a
Erik Lokhorst <erik@pitts2.onmicrosoft.com>
2024-12-08 22:59:31 +0100

    day8 part 2 v1

--- 2024/day08.py
+++ 2024/day08.py
@@ -1,18 +1,36 @@
 from itertools import combinations
 
 
-def calculate_new_coordinates(comb):
+def calc_top_coords(comb, i):
+    return (
+        comb[0][0] - (comb[1][0] - comb[0][0]) * i,
+        comb[0][1] + (comb[0][1] - comb[1][1]) * i,
+    )
+
+
+def calc_bot_coords(comb, i):
+    return (
+        comb[1][0] + (comb[1][0] - comb[0][0]) * i,
+        comb[1][1] - (comb[0][1] - comb[1][1]) * i,
+    )
+
+
+def calculate_new_coordinates(comb, max_x, max_y):
     """Calculate new coordinates based on the given combination."""
-    new_coords = [
-        (
-            comb[0][0] - (comb[1][0] - comb[0][0]),
-            comb[0][1] + (comb[0][1] - comb[1][1]),
-        ),
-        (
-            comb[1][0] + (comb[1][0] - comb[0][0]),
-            comb[1][1] - (comb[0][1] - comb[1][1]),
-        ),
-    ]
+    new_coords = []
+    i = 1
+    new_coords.append(calc_bot_coords(comb, i))
+    while max_x >= new_coords[-1][0] >= 0 and max_y >= new_coords[-1][1] >= 0:
+        i += 1
+        new_coords.append(calc_bot_coords(comb, i))
+
+    i = 1
+    new_coords.append(calc_top_coords(comb, i))
+    while max_x >= new_coords[-1][0] >= 0 and max_y >= new_coords[-1][1] >= 0:
+        i += 1
+        new_coords.append(calc_top_coords(comb, i))
+
+    new_coords.extend([cd for cd in comb])
     return new_coords
 
 
@@ -32,7 +50,7 @@ def main(file: str):
             combs = list(combinations(coords, 2))
 
             for comb in combs:
-                for new in calculate_new_coordinates(comb):
+                for new in calculate_new_coordinates(comb, len(lines[0]), len(lines)):
                     if new in all_coords:
                         antisignals.add(new)
 
