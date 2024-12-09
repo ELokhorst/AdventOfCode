bc5e0c32da9259e51d5ae77485794399a46e343c
Erik Lokhorst <erik@pitts2.onmicrosoft.com>
2024-12-09 21:07:55 +0100

    finish day9

--- 2024/day09.py
+++ 2024/day09.py
@@ -2,46 +2,34 @@ def main(file: str):
     with open(file, "r", encoding="utf-8") as f:
         disk = f.read().strip()
 
-    files = [(id, size) for id, size in enumerate(map(int, disk[::2]))]
-    free = [(id, size) for id, size in enumerate(list(map(int, disk[1::2])))]
-    files_list = [file[1] * [file[0]] for file in files]
-    print(files_list)
-    files_list.reverse()
+    files_list = []
+    num = 0
+    for i, size in enumerate(map(int, disk)):
+        if i % 2 == 0:  # file
+            files_list.append(size * [num])
+            num += 1
+        else:  # free space
+            files_list.append(size * [-1])
 
-    new_order = [files_list[-1]]
-    stack = []
-    for file in files_list[:-1]:
-        print(f"File: {file}")
-
+    for file in files_list[::-1][:-1][::2]:
         suitable_space = None
-        for index, size in free:
-            if len(file) <= size:
-                suitable_space = (index, size)
+        free = [(i, l) for i, l in enumerate(files_list) if any(val == -1 for val in l)]
+        for index, empty_list in free:
+            if len(file) <= empty_list.count(-1):
+                suitable_space = (index, empty_list)
                 break
-        print(suitable_space)
 
         if suitable_space:
-            remaining_space = suitable_space[1] - len(file)
-            free[suitable_space[0]] = (
-                suitable_space[0] + 1,
-                remaining_space,
-            )
-            files_list.remove(file)
-            new_order.insert(suitable_space[0] + 1, file)
-        else:
-            stack.append(file)
-        print(new_order)
+            index_start = suitable_space[1].index(-1)
+            suitable_space[1][index_start : index_start + len(file)] = file
+            files_list[suitable_space[0]] = suitable_space[1]
+            last_index = len(files_list) - 1 - files_list[::-1].index(file)
+            files_list[last_index] = [-1] * len(file)
 
-    for file in stack:
-        new_order.append(file)
-    print(new_order)
-
-    print(free)
-    for idx, space in free:
-        new_order.insert(idx + 2, [0] * space)
-    print(new_order)
-
-    checksum = [i * int(item) for i, item in enumerate(new_order)]
+    flattened_list = [
+        int(item) if item != -1 else 0 for file in files_list for item in file
+    ]
+    checksum = [i * x for i, x in enumerate(flattened_list)]
     return sum(checksum)
 
 
