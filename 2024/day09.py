1612760bc7110270c656a9d6414d7f4fd169e73d
Erik Lokhorst <erik@pitts2.onmicrosoft.com>
2024-12-09 19:14:09 +0100

    day9 part 2 unfinished

--- 2024/day09.py
+++ 2024/day09.py
@@ -1,26 +1,45 @@
-from collections import Counter
-
-
 def main(file: str):
     with open(file, "r", encoding="utf-8") as f:
         disk = f.read().strip()
 
     files = [(id, size) for id, size in enumerate(map(int, disk[::2]))]
-    free = list(map(int, disk[1::2]))
-    files_list = [int(x) for file in files for x in file[1] * [file[0]]]
+    free = [(id, size) for id, size in enumerate(list(map(int, disk[1::2])))]
+    files_list = [file[1] * [file[0]] for file in files]
+    print(files_list)
+    files_list.reverse()
 
-    new_order = []
-    for i, size in enumerate(free):
-        if len(files_list) > size:
-            new_order.extend(min(files[i][1], len(files_list)) * [files[i][0]])
-            files_list = files_list[files[i][1] :]
+    new_order = [files_list[-1]]
+    stack = []
+    for file in files_list[:-1]:
+        print(f"File: {file}")
 
-            for _ in range(size):
-                if files_list:
-                    new_order.append(files_list.pop())
+        suitable_space = None
+        for index, size in free:
+            if len(file) <= size:
+                suitable_space = (index, size)
+                break
+        print(suitable_space)
+
+        if suitable_space:
+            remaining_space = suitable_space[1] - len(file)
+            free[suitable_space[0]] = (
+                suitable_space[0] + 1,
+                remaining_space,
+            )
+            files_list.remove(file)
+            new_order.insert(index + 1, file)
         else:
-            while files_list:
-                new_order.append(files_list.pop())
+            stack.append(file)
+        print(new_order)
+
+    for file in stack:
+        new_order.append(file)
+    print(new_order)
+
+    print(free)
+    for idx, space in free:
+        new_order.insert(idx + 2, [0] * space)
+    print(new_order)
 
     checksum = [i * int(item) for i, item in enumerate(new_order)]
     return sum(checksum)
