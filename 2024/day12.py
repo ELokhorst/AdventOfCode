46f2babe9b0d9391e6b576a740b15877687c8485
Erik Lokhorst <erik@pitts2.onmicrosoft.com>
2024-12-12 11:54:42 +0100

    optimize

--- 2024/day12.py
+++ 2024/day12.py
@@ -1,3 +1,26 @@
+def find_regions(coord_set: set):
+    regions = []
+    directions = [(0, 1), (0, -1), (1, 0), (-1, 0)]
+
+    while coord_set:
+        x, y, char = coord_set.pop()
+        region = [(x, y, char)]
+        stack = [(x, y, char)]
+
+        while stack:
+            cx, cy, cchar = stack.pop()
+            for dx, dy in directions:
+                neighbor = (cx + dx, cy + dy, cchar)
+                if neighbor in coord_set:
+                    coord_set.remove(neighbor)
+                    stack.append(neighbor)
+                    region.append(neighbor)
+
+        regions.append(region)
+
+    return regions
+
+
 def count_surrounding_lots(lot, coord):
     x, y, char = coord
     p = [(0, 1), (0, -1), (1, 0), (-1, 0)]
@@ -10,42 +33,17 @@ def main(file: str):
         layout = f.read().strip()
 
     lines = layout.splitlines()
-    coords = []
+    coords = set()
     for y, line in enumerate(lines):
         for x, char in enumerate(line):
-            coords.append((x, y, char))
+            coords.add((x, y, char))
 
-    visited = set()
-    regions = []
-    directions = [(0, 1), (0, -1), (1, 0), (-1, 0)]
-    while coords:
-        x, y, char = coords[0]
-        if (x, y, char) in visited:
-            continue
-        visited.add((x, y, char))
-        coords.remove((x, y, char))
-
-        region = [(x, y, char)]
-        found_region = True
-        while found_region is True:
-            found_region = False
-            for x, y, char in region:
-                for d in directions:
-                    new_pos = (x + d[0], y + d[1], char)
-                    if new_pos in coords:
-                        region += [new_pos]
-                        coords.remove(new_pos)
-                        found_region = True
-
-        regions.append(region)
-    print(regions)
+    regions = find_regions(coords)
 
     sizes = [len(lots) for lots in regions]
-    print(sizes)
     perimeters = [
         sum(count_surrounding_lots(lots, coord) for coord in lots) for lots in regions
     ]
-    print(perimeters)
     costs = [a * b for a, b in zip(sizes, perimeters)]
     return sum(costs)
 
