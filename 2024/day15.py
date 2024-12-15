a53b345dfda83867e510b86f8f24761fe47c6fff
Erik Lokhorst <erik@pitts2.onmicrosoft.com>
2024-12-15 22:44:58 +0100

    day15 part 1

---
+++ 2024/day15.py
@@ -0,0 +1,46 @@
+def read_layout(layout) -> dict:
+    coords = {(x, y): char for y, l in enumerate(layout) for x, char in enumerate(l)}
+    return coords
+
+
+def update_coords(coords: dict, instruction: str):
+    current_position = [k for k, v in coords.items() if v == "@"][0]
+    dy = -1 if instruction == "^" else 1 if instruction == "v" else 0
+    dx = 1 if instruction == ">" else -1 if instruction == "<" else 0
+    next_pos = (current_position[0] + dx, current_position[1] + dy)
+    print(next_pos)
+    if coords[next_pos] == ".":
+        coords[next_pos] = "@"
+        coords[current_position] = "."
+    elif coords[next_pos] == "O":
+        move = [next_pos]
+        while coords[next_pos] == "O":
+            next_pos = (next_pos[0] + dx, next_pos[1] + dy)
+            move.append(next_pos)
+        if coords[next_pos] == ".":
+            coords[current_position] = "."
+            coords[move.pop(0)] = "@"
+            for coord in move:
+                coords[coord] = "O"
+    return coords
+
+
+def main(file: str):
+    with open(file, "r", encoding="utf-8") as f:
+        content = f.read().strip().split("\n\n")
+
+    coords, instructions = read_layout(content[0].splitlines()), list(content[1])
+    while instructions:
+        instruction = instructions.pop(0)
+        print(instruction)
+        coords = update_coords(coords, instruction)
+
+    return sum(
+        [100 * coord[1] + coord[0] for coord, val in coords.items() if val == "O"]
+    )
+
+
+res_example = main("2024/day15_example.txt")
+print(res_example)
+res_actual = main("2024/day15_input.txt")
+print(res_actual)
