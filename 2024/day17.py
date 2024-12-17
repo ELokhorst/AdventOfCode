7bd2e55baf6dbcb203c76145e2c65b318a4d3e54
Erik Lokhorst <erik@pitts2.onmicrosoft.com>
2024-12-17 16:42:16 +0100

    small improvement

--- 2024/day17.py
+++ 2024/day17.py
@@ -3,19 +3,20 @@ from re import findall
 
 def main(file: str):
     with open(file, "r", encoding="utf-8") as f:
-        a, b, c, *program = [int(n) for n in findall("(\d+)", f.read())]
+        a, b, c, *program = [int(n) for n in findall(r"(\d+)", f.read())]
 
     out = []
     i = 0
     while 0 <= i < len(program):
+        instruction = program[i]
         operand = program[i + 1]
         combo_op = [0, 1, 2, 3, a, b, c, None][operand]
 
-        print(f"Opcode: {program[i]}. Operand: {operand}. Combo_op: {combo_op}")
+        print(f"Instruction: {instruction}. Operand: {operand}. Combo_op: {combo_op}")
 
-        match (program[i]):
+        match (instruction):
             case 0:
-                a = int(a / 2**combo_op)
+                a = a >> combo_op
             case 1:
                 b = b ^ operand
             case 2:
@@ -27,16 +28,20 @@ def main(file: str):
             case 5:
                 out.append(combo_op % 8)
             case 6:
-                b = int(a / 2**combo_op)
+                b = a >> combo_op
             case 7:
-                c = int(a / 2**combo_op)
+                c = a >> combo_op
 
         i += 2
+        print(a)
+        print(b)
+        print(c)
+        print(out)
 
     return ",".join([str(val) for val in out])
 
 
 res_example = main("2024/day17_example.txt")
 print(res_example)
-# res_actual = main("2024/day17_input.txt")
-# print(res_actual)
+res_actual = main("2024/day17_input.txt")
+print(res_actual)
