9b43b0cffedd094d45643abb3d4eac6a9a68fc91
Erik Lokhorst <erik@pitts2.onmicrosoft.com>
2024-12-18 11:26:53 +0100

    day17 part 2

--- 2024/day17.py
+++ 2024/day17.py
@@ -1,10 +1,8 @@
 from re import findall
+from z3 import BitVec, Optimize
 
 
-def main(file: str):
-    with open(file, "r", encoding="utf-8") as f:
-        a, b, c, *program = [int(n) for n in findall(r"(\d+)", f.read())]
-
+def run(program, a=0, b=0, c=0):
     out = []
     i = 0
     while 0 <= i < len(program):
@@ -12,7 +10,7 @@ def main(file: str):
         operand = program[i + 1]
         combo_op = [0, 1, 2, 3, a, b, c, None][operand]
 
-        print(f"Instruction: {instruction}. Operand: {operand}. Combo_op: {combo_op}")
+        # print(f"Instruction: {instruction}. Operand: {operand}. Combo_op: {combo_op}")
 
         match (instruction):
             case 0:
@@ -33,12 +31,34 @@ def main(file: str):
                 c = a >> combo_op
 
         i += 2
-        print(a)
-        print(b)
-        print(c)
-        print(out)
-
     return ",".join([str(val) for val in out])
+
+
+def optimize(program):
+    opt = Optimize()
+    s = BitVec("s", 64)
+    a, b, c = s, 0, 0
+    for x in program:
+        b = a % 8
+        b = b ^ 5
+        c = a >> b
+        b = b ^ c
+        b = b ^ 6
+        a = a >> 3
+        opt.add((b % 8) == x)
+    opt.add(a == 0)
+    opt.minimize(s)
+    assert str(opt.check()) == "sat"
+    print(opt.model().eval(s))
+
+
+def main(file: str):
+    with open(file, "r", encoding="utf-8") as f:
+        a, b, c, *program = [int(n) for n in findall(r"(\d+)", f.read())]
+
+    out = run(program, a, b, c)
+    optimize(program)
+    return out
 
 
 res_example = main("2024/day17_example.txt")
