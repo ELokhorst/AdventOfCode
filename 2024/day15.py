e5e91f8e22dd0e69da4948c5f0bd85ab319b2704
Erik Lokhorst <erik@pitts2.onmicrosoft.com>
2024-12-19 13:46:42 +0100

    clean up code

--- 2024/day15.py
+++ 2024/day15.py
@@ -27,23 +27,27 @@ def get_boxes_to_move(coords: dict, instruction: str, robot_pos: tuple):
     dx = 1 if instruction == ">" else -1 if instruction == "<" else 0
 
     stack = [robot_pos]
+    visited = set()
     i = 0
+
     while i < len(stack):
         x, y = stack[i]
-        nx, ny = (x + dx, y + dy)
-        cchar = coords[(nx, ny)]
+        nx, ny = x + dx, y + dy
+        cchar = coords.get((nx, ny), None)
 
         if cchar == "#":
             return [], (0, 0)
 
-        if (nx, ny) not in set(stack) and cchar in "O[]":
+        if (nx, ny) not in visited and cchar in "O[]":
             stack.append((nx, ny))
+            visited.add((nx, ny))
             if cchar == "]":
                 stack.append((nx - 1, ny))
             elif cchar == "[":
                 stack.append((nx + 1, ny))
 
         i += 1
+
     return stack, (dx, dy)
 
 
@@ -51,22 +55,21 @@ def main(file: str):
     with open(file, "r", encoding="utf-8") as f:
         content = f.read().strip().split("\n\n")
 
-    coords, instructions = read_layout(content[0].splitlines()), list(content[1])
-    robot_pos = [k for k, v in coords.items() if v == "@"][0]
-    i = 0
-    while i < len(instructions):
-        instruction = instructions[i]
-        boxes2move, (dx, dy) = get_boxes_to_move(coords, instruction, robot_pos)
-        if boxes2move:
-            for x, y in boxes2move[::-1]:
+    coords = read_layout(content[0].splitlines())
+    instructions = list(content[1])
+    robot_pos = next(k for k, v in coords.items() if v == "@")
+
+    for instruction in instructions:
+        boxes_to_move, (dx, dy) = get_boxes_to_move(coords, instruction, robot_pos)
+        if boxes_to_move:
+            for x, y in reversed(boxes_to_move):
                 coords[x + dx, y + dy] = coords[x, y]
                 coords[x, y] = "."
-            robot_pos = (x + dx, y + dy)
-        i += 1
+            robot_pos = (robot_pos[0] + dx, robot_pos[1] + dy)
 
     print_grid(coords)
     result = sum(
-        [100 * coord[1] + coord[0] for coord, val in coords.items() if val == "["]
+        100 * coord[1] + coord[0] for coord, val in coords.items() if val == "["
     )
     return result
 
