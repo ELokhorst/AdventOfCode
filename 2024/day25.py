9b9d0ecfe6acad98f7aee83071f6cfdb98200445
Erik Lokhorst <erik@pitts2.onmicrosoft.com>
2024-12-25 10:43:00 +0100

    day 25 part 1

--- 2024/day25.py
+++ 2024/day25.py
@@ -6,18 +6,34 @@ def main(file: str):
         content = f.read().strip().replace(".", "0").replace("#", "1")
 
     schematics = [
-        [list(map(int, list(line))) for line in schema.splitlines()]
+        np.array(
+            [list(map(int, list(line))) for line in schema.splitlines()]
+        ).T.tolist()
         for schema in content.split("\n\n")
     ]
 
-    for schema in schematics:
-        print(np.array(schema).T.tolist())
-        print("\n")
+    locks = [
+        [line for line in schematic]
+        for schematic in schematics
+        if all(line[0] == 1 for line in schematic)
+    ]
+    keys = [
+        [line for line in schematic]
+        for schematic in schematics
+        if all(line[0] == 0 for line in schematic)
+    ]
 
-    return schematics
+    fit = 0
+    for lock in locks:
+        for key in keys:
+            overlap_matrix = sum([np.array(lock), np.array(key)])
+            if np.all(overlap_matrix <= 1):
+                fit += 1
+
+    return fit
 
 
 res_example = main("2024/day25_example.txt")
 print(res_example)
-# res_actual = main("2024/day25_input.txt")
-# print(res_actual)
+res_actual = main("2024/day25_input.txt")
+print(res_actual)
