f84c1e0d112ba90fc29f365a5c4bb1d2cd78bcc8
Erik Lokhorst <erik@pitts2.onmicrosoft.com>
2024-12-26 11:54:43 +0100

    day 22 part 2

--- 2024/day22.py
+++ 2024/day22.py
@@ -7,11 +7,8 @@ def prune(secretno: int):
 
 
 def calc_next_secret(n: int):
-    # Step 1
     n = prune(mix(n, n * 64))
-    # Step 2
     n = prune(mix(n, n // 32))
-    # Step 3
     n = prune(mix(n, n * 2048))
     return n
 
@@ -33,6 +30,17 @@ def generate_secrets(tracker: dict, target: int, initial_value: int):
     return secrets
 
 
+def add_sequence(sequences: dict, sublists: list[tuple], prices):
+    seen = set()
+    for tupl, price in zip(sublists, prices):
+        if tupl not in seen:
+            seen.add(tupl)
+            if tupl not in sequences:
+                sequences[tupl] = price
+            else:
+                sequences[tupl] += price
+
+
 def main(file: str):
     with open(file, "r", encoding="utf-8") as f:
         lines = f.read().strip().splitlines()
@@ -45,19 +53,21 @@ def main(file: str):
         secrets = generate_secrets(tracker, target, buyer)
         secrets_per_number.append(secrets)
 
-    for price in range(1, 10):
-        print(price)
-        for buyer in buyers:
-            secrets = generate_secrets(tracker, target, buyer)
-            prices = [secret % 10 for secret in secrets]
-            diffs = [0] + [prices[i + 1] - prices[i] for i in range(len(prices) - 1)]
-            # Find optimal sequence for given price & buyer
+    sequences = {}
+    for buyer in buyers:
+        secrets = generate_secrets(tracker, target, buyer)
+        prices = [secret % 10 for secret in secrets]
+        diffs = [prices[i + 1] - prices[i] for i in range(len(prices) - 1)]
+        sublists = [tuple(diffs[i - 4 : i]) for i in range(4, len(diffs))]
+        add_sequence(sequences, sublists, prices[4:])
 
-    lastnum = sum([l[-1] for l in secrets_per_number])
-    return diffs
+    best_sequence = max(sequences.items(), key=lambda x: x[1])
+
+    lastnum = sum([l[-1] for l in secrets_per_number])  # Part 1
+    return lastnum, best_sequence
 
 
 res_example = main("2024/day22_example.txt")
 print(res_example)
-# res_actual = main("2024/day22_input.txt")
-# print(res_actual)
+res_actual = main("2024/day22_input.txt")
+print(res_actual)
